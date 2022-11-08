cfc876326e6ff1176d1cb7cde6dce9381c95f0b4
Joao Bruno Freitas Vasconcelos <joaobruno.freitas@uece.br>
2022-11-08 15:12:08 -0300

    Criando o DTO do relatório

--- src/main/java/com/example/algamoney/api/model/TipoLancamento.java
+++ src/main/java/com/example/algamoney/api/model/TipoLancamento.java
@@ -1,6 +1,16 @@
 package com.example.algamoney.api.model;
 
 public enum TipoLancamento {
-	RECEITA,
-	DESPESA
+	RECEITA("Receita"),
+	DESPESA("Despesa");
+	
+	private final String descricao;
+	
+	TipoLancamento(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
 }
