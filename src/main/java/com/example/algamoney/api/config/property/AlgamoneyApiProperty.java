ffcfea1640fd3aa2eb9508862bb09da21db0798a
Joao Bruno Freitas Vasconcelos <joaobruno.freitas@uece.br>
2022-11-21 12:49:00 -0300

    Configurando o envio de e-mail

--- src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/example/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -13,6 +13,12 @@ public class AlgamoneyApiProperty {
 	
 	private final Seguranca seguranca = new Seguranca();
 	
+	private final Mail mail = new Mail();
+	
+	public Mail getMail() {
+		return mail;
+	}
+	
 	public Seguranca getSeguranca() {
 		return seguranca;
 	}
@@ -35,6 +41,45 @@ public class AlgamoneyApiProperty {
 
 		public void setEnableHttps(boolean enableHttps) {
 			this.enableHttps = enableHttps;
+		}		
+	}
+	
+	public static class Mail {
+		private String host;
+		private Integer port;
+		private String username;
+		private String password;
+		
+		public String getHost() {
+			return host;
+		}
+		
+		public void setHost(String host) {
+			this.host = host;
+		}
+		
+		public Integer getPort() {
+			return port;
+		}
+		
+		public void setPort(Integer port) {
+			this.port = port;
+		}
+		
+		public String getUsername() {
+			return username;
+		}
+		
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		
+		public String getPassword() {
+			return password;
+		}
+		
+		public void setPassword(String password) {
+			this.password = password;
 		}		
 	}
 }
