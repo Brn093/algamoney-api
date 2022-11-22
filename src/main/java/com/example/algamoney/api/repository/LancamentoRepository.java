4237b3c18e983b5278843b471afedfaf9b6b172f
Joao Bruno Freitas Vasconcelos <joaobruno.freitas@uece.br>
2022-11-22 11:45:41 -0300

    Buscando lançamentos vencidos com Spring Data JPA

--- src/main/java/com/example/algamoney/api/repository/LancamentoRepository.java
+++ src/main/java/com/example/algamoney/api/repository/LancamentoRepository.java
@@ -1,5 +1,8 @@
 package com.example.algamoney.api.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +11,6 @@ import com.example.algamoney.api.repository.lancamento.LancamentoRepositoryQuery
 
 @Repository
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
-
+	
+	List<Lancamento> findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate data);
 }
